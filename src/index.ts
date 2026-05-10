416056f5fdbdab05847449559037b568b04bf274
Ombo <ombo@Ombos-Mackbook.local>
2026-05-10 19:20:21 +0800

    added port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Elysia } from "elysia";
+import env from "./env";
 import { usersController } from "./routes/user/controller";
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
   .use(usersController)
-  .listen(3000);
+  .listen(env.PORT);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
